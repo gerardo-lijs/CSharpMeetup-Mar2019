2a37ad3c05483591536a5790059d46dc2f25f959
Gerardo Lijs <gerardo.lijs@gmail.com>
2019-04-22 09:13:40 +0200

    Use MahApps and ReactveUI

--- demo/03-AsyncProgressDesktop/AsyncProgressDesktop/ViewModels/MainWindowViewModel.cs
+++ demo/03-AsyncProgressDesktop/AsyncProgressDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace AsyncProgressDesktop.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ReactiveObject
     {
+        // Commands
+        public ReactiveCommand<Unit, Unit> CalculatePrimeNumbers { get; }
+
+        private int _StartNumber = 1;
+        public int StartNumber
+        {
+            get => _StartNumber;
+            set => this.RaiseAndSetIfChanged(ref _StartNumber, value);
+        }
+
+        private int _EndNumber = 5000000;
+        public int EndNumber
+        {
+            get => _EndNumber;
+            set => this.RaiseAndSetIfChanged(ref _EndNumber, value);
+        }
+
+        private string _PrimesCountResultText;
+        public string PrimesCountResultText
+        {
+            get => _PrimesCountResultText;
+            private set => this.RaiseAndSetIfChanged(ref _PrimesCountResultText, value);
+        }
+
+        private bool _IsCalculating;
+        public bool IsCalculating
+        {
+            get => _IsCalculating;
+            private set => this.RaiseAndSetIfChanged(ref _IsCalculating, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            // Create command
+            CalculatePrimeNumbers = ReactiveCommand.CreateFromTask(CalculatePrimeNumbersImpl);
+
+            //_isAvailable = this
+            //           .WhenAnyValue(x => x.SearchResults)
+            //           .Select(searchResults => searchResults != null)
+            //           .ToProperty(this, x => x.IsAvailable);
+        }
+
+        private async Task CalculatePrimeNumbersImpl()
+        {
+            // Notify UI that calculation is in progress
+            IsCalculating = true;
+
+            // Calculate
+            int primesCount = await Task.Run(() =>
+                 ParallelEnumerable.Range(StartNumber, EndNumber).Count(n => Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+
+            PrimesCountResultText = $"{primesCount} prime numbers between {StartNumber} and {EndNumber}";
+
+            // Notify UI that calculation is finished
+            IsCalculating = false;
+        }
     }
 }
--- demo/03-AsyncProgressDesktop/AsyncProgressDesktop/Views/MainWindow.xaml.cs
+++ demo/03-AsyncProgressDesktop/AsyncProgressDesktop/Views/MainWindow.xaml.cs
@@ -1,45 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using AsyncProgressDesktop.ViewModels;
+using ReactiveUI;
+using System.Reactive.Disposables;
 
 namespace AsyncProgressDesktop.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
+            ViewModel = new MainWindowViewModel();
+            this.WhenActivated(disposableRegistration =>
+            {
+                //// Notice we don't have to provide a converter, on WPF a global converter is
+                //// registered which knows how to convert a boolean into visibility.
+                //this.OneWayBind(ViewModel, viewModel => viewModel.IsAvailable, view => view.searchResultsListBox.Visibility)
+                //    .DisposeWith(disposableRegistration);
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            int.TryParse(StartNumberTextBox.Text, out int start);
-            int.TryParse(EndNumberTextBox.Text, out int end);
-            if (start == 0 || end == 0) return;
+                // Values
+                this.Bind(ViewModel, viewModel => viewModel.StartNumber, view => view.StartNumberNumericUpDown.Value, value => value, value => (int)value).DisposeWith(disposableRegistration);
+                this.Bind(ViewModel, viewModel => viewModel.EndNumber, view => view.EndNumberNumericUpDown.Value, value => value, value => (int)value).DisposeWith(disposableRegistration);
+                this.OneWayBind(ViewModel, viewModel => viewModel.PrimesCountResultText, view => view.ResultTextBlock.Text).DisposeWith(disposableRegistration);
 
-            ResultTextBlock.Text = "";
-            int result = await GetPrimesCountAsync(start, end);
-            ResultTextBlock.Text = $"{result} prime numbers between {start} and {end}";
-        }
-
-        private Task<int> GetPrimesCountAsync(int start, int count)
-        {
-            return Task.Run(() =>
-                ParallelEnumerable.Range(start, count).Count(n => Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+                // Commands
+                this.BindCommand(ViewModel, viewModel => viewModel.CalculatePrimeNumbers, view => view.CalculatePrimeNumbersButton).DisposeWith(disposableRegistration);
+            });
         }
     }
 }
